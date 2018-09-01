69bd2d37500531ae035c34bcdf9b5cf97b11804c
Einar Ólafsson <einar.olafsson@koerber.digital>
2018-09-01 15:48:54 +0200

    Add files

---
+++ index.js
@@ -0,0 +1,55 @@
+var textract = require('textract');
+var WordExtractor = require("word-extractor");
+
+const searchErrorFile = (file) => {
+  var extractor = new WordExtractor();
+  var extracted = extractor.extract(file);
+  extracted.then(function(doc) {
+    var body = doc.getBody()
+    if(body.indexOf('Vignemont') >= 0 || body.indexOf('vignemont')  >= 0) {
+      console.log(file);
+    }
+  })
+  .catch((err) => console.log('Could not read this file >>>', file));
+
+}
+
+const serachFile = (file) => {
+    textract.fromFileWithPath(file, (error, text) => {
+      if(error) {
+        // console.log('PROBLEM', file)
+        searchErrorFile(file)
+      } else {
+        if(text.indexOf('Vignemont') >= 0 || text.indexOf('vignemont')  >= 0) {
+          console.log(file);
+        }
+      }
+    })
+
+}
+
+var path = require('path'), fs=require('fs');
+
+function fromDir(startPath,filter){
+
+    //console.log('Starting from dir '+startPath+'/');
+
+    if (!fs.existsSync(startPath)){
+        console.log("no dir ",startPath);
+        return;
+    }
+
+    var files=fs.readdirSync(startPath);
+    for(var i=0;i<files.length;i++){
+        var filename=path.join(startPath,files[i]);
+        var stat = fs.lstatSync(filename);
+        if (stat.isDirectory()){
+            fromDir(filename,filter); //recurse
+        }
+        else if (filename.indexOf(filter)>=0) {
+            serachFile('./' + filename)
+        };
+    };
+};
+
+fromDir('./files','.doc');
