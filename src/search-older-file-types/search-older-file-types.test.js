7ba842d3955c3efbe0d4cf819212b4ecb44bc353
Einar Ólafsson <einar.olafsson@koerber.digital>
2018-09-24 17:17:59 +0200

    Update test for older file search

--- src/search-older-file-types/search-older-file-types.test.js
+++ src/search-older-file-types/search-older-file-types.test.js
@@ -2,9 +2,9 @@ import test from 'ava'
 
 import searchOlderFileTypes from './search-older-file-types'
 
-test('Able to read doc format', async t => {
+test('Able to read .doc format', async t => {
   const file = 'test_source/the-great-sea.doc'
-  const searchTerms = ['not', 'where']
+  const searchTerms = ['dataarartsadfas']
   let found = null
   try {
     found = await searchOlderFileTypes({file, searchTerms})
@@ -12,7 +12,14 @@ test('Able to read doc format', async t => {
   catch (error) {
     t.fail(error)
   }
+  finally {
+    if (!found) {
+      t.fail('Could not find content within test file')
+    }
 
-  t.pass(found)
+    t.pass()
+  }
+
+  
 
 })
